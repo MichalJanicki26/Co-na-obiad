f0973bd4df9606ac177ee540e2e8427ed7405447
MichalJanicki26 <michaljanicki26@gmail.com>
2023-02-21 14:46:02 +0100

    fight with displaying recipe

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
 {
+    const mainContainer = document.querySelector(".jsMain")
     const btn = document.querySelector(".jsBtn");
     const recipeContainer = document.querySelector(".jsRecipe");
-    
+
     btn.addEventListener("click", () => {
         const cookBookStringValue = window.localStorage.getItem("cookBook");
+        
+        const firstTitle = cookBookStringValue.map(({ title }) => title );
+
+        mainContainer.innerHTML = `
+        <div class="recipe jsRecipe">
+        <div class="recipe__title">${firstTitle}</div>
+        <div class="jsRecipe__ingredientList recipe__ingredientList"></div>
+        <div class="jsRecipe__instructions recipe__instructions"></div>
+        </div>
+        `;
         console.log(cookBookStringValue);
     });
 }
